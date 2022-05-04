4200be33a1c85a5f5e1c667b2ccb266201b14f02
Salia-Monica <saliamonica78@gmail.com>
2022-05-04 13:55:09 +0000

    first commit

---
+++ js/subtract.text.js
@@ -0,0 +1,6 @@
+const subtract = require("./subtract")
+
+Text("subtract two numbers", () => {
+    expect(subtract(2, 1)
+    ).toBe(1)
+})
---
+++ js/sum.test.js
@@ -0,0 +1,8 @@
+const sum = require("./sum")
+
+test("properly adding two numbers", () => {
+    expect(sum(1, 2)
+    ).toBe(3)
+
+  
+})
